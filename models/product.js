744e79876132f02226362cc91369c6f9b1fc3427
Stephen Muñez <sjtmunez@gmail.com>
2024-08-07 13:25:35 +0800

    feat: populate db

--- models/product.js
+++ models/product.js
@@ -1,30 +1,29 @@
 const mongoose = require('mongoose');
 
-const productSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    featured: {
+      type: Boolean,
+      default: false,
+    },
+    rating: {
+      type: Number,
+      default: 4.5,
+    },
+    company: {
+      type: String,
+      enum: ['ikea', 'liddy', 'caressa', 'marcos'],
+    },
   },
-  price: {
-    type: Number,
-    required: true,
-  },
-  featured: {
-    type: Boolean,
-    default: false,
-  },
-  rating: {
-    type: Number,
-    default: 4.5,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-  },
-  company: {
-    type: String,
-    enum: ['ikea', 'liddy', 'caressa', 'marcos'],
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model('Product', productSchema);
