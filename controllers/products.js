bbc50abcd79b6dc5d3c7dccf7043a778c67ff674
Stephen Muñez <sjtmunez@gmail.com>
2024-08-08 14:28:59 +0800

    feat: company filter

--- controllers/products.js
+++ controllers/products.js
@@ -8,11 +8,15 @@ const getAllProductsStatic = async (req, res) => {
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured } = req.query;
+  const { featured, company } = req.query;
   const queryObject = {};
 
   if (featured) {
     queryObject.featured = featured.toLowerCase() === 'true';
+  }
+
+  if (company) {
+    queryObject.company = company;
   }
 
   console.log(queryObject);
