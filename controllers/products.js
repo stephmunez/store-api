82ed464f928147ada73c42d2586736b0135de895
Stephen Muñez <sjtmunez@gmail.com>
2024-08-12 11:56:53 +0800

    feat: limit and skip functionality

--- controllers/products.js
+++ controllers/products.js
@@ -6,7 +6,7 @@ const getAllProductsStatic = async (req, res) => {
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort, fields } = req.query;
+  let { featured, company, name, sort, fields, page, limit } = req.query;
   const queryObject = {};
 
   if (featured) {
@@ -35,9 +35,21 @@ const getAllProducts = async (req, res) => {
     result = result.select(fieldsList);
   }
 
+  const { skip, limit: finalLimit } = getPagination(page, limit);
+
+  result = result.skip(skip).limit(finalLimit);
+
   const products = await result;
 
   res.send({ products, length: products.length });
+};
+
+const getPagination = (page, limit) => {
+  page = Number(page) || 1;
+  limit = Number(limit) || 10;
+
+  const skip = (page - 1) * limit;
+  return { skip, limit };
 };
 
 module.exports = { getAllProductsStatic, getAllProducts };
